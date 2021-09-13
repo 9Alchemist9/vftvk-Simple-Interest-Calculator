290c72302fb0d50d70a6ad5e51e723ccdf44028f
9Alchemist9 <alckmindiego@gmail.com>
2021-09-13 18:26:33 +0000

    changes made from theia lab - Introduction to Web Development Course

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+		{   
+            // Set the reference to the amount variable;
+
+        	var principal = document.getElementById("principal").value;
+        
+            /* Establishes the condition to execute the operation. If amount is zero or
+            a negative number, it returns an alert and reset the input field; */
+
+        	if(principal <= 0){
+            alert("Insert a positive number!"); // Shows an alert
+            var resetprincipal = document.getElementById('principal'); // Set the reference for the variable;
+        	resetprincipal.value = resetprincipal.defaultValue; // Sets the default value to the amount input field ;
+            Document.getElementById("principal").focus; // Sets focus at the amount input field;
+            
+            } else {
+        
+            /* If the amount field value is a positive number; */    
+    		
+    		var rate = document.getElementById("rate").value; // Defines a variable to the interest rate value;
+    		var years = document.getElementById("years").value; // Defines a variable to the number of years;
+    		var interest = principal * years * rate / 100; // Defines a variable to the calculation value;
+    		var year = new Date().getFullYear()+parseInt(years); // Function to calculate the year, based on the actual;
+
+            // Defines the output message shown after the calculation and includes its variable values;
+
+            document.getElementById("result").innerHTML = "If you deposit <mark>"+ principal +"</mark>, \
+            <br\> at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of 							<mark>"+interest+"</mark>,\
+            <br\>in the year <mark>"+year+"</mark>\<br\>"
+		}
+        }
+
+        // Function to link the span content with the range input value;
+
+    	function updateRate() {
+        		var rateval = document.getElementById("rate").value;
+        		document.getElementById("rate_val").innerText = rateval;
+    	}
         
